3c51b9e4bb78fd765c89332633a90d350a3fce29
shawt <shawtyhead@mail.ru>
2025-02-13 23:06:12 +0300

    parameterized test is added

--- src/test/java/com/skypro/unit_testing_hw/service/CalculatorServiceImplParameterizedTestt.java
+++ src/test/java/com/skypro/unit_testing_hw/service/CalculatorServiceImplParameterizedTestt.java
@@ -8,7 +8,7 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CalculatorServiceImplParameterizedTest {
+public class CalculatorServiceImplParameterizedTestt {
     private final CalculatorServiceImpl calculatorService = new CalculatorServiceImpl();
 
     public static Stream<Arguments> argumentsStream() {
